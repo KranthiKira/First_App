1525c840a457669dfe5112969bed605abfb376f2
Kranthi Kiran Reddy Beduduri <kirankranthi1008@gmail.com>
2025-11-11 23:56:17 +0530

    kranthi_kiran_project

---
+++ src/main/java/com/first_project/kranthi_kiran/KranthiKiranApplication.java
@@ -0,0 +1,13 @@
+package com.first_project.kranthi_kiran;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KranthiKiranApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KranthiKiranApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/first_project/kranthi_kiran/KranthiKiranApplicationTests.java
@@ -0,0 +1,13 @@
+package com.first_project.kranthi_kiran;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KranthiKiranApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
